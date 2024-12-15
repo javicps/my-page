0ad299ea6ae5dd479eecb58a40c4fa245620d713
javicps <javicps@gmail.com>
2024-12-15 15:50:05 +0100

    Add about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   return (
@@ -9,14 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to My Page</h1>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       </header>
     </div>
   );
