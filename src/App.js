59aac620dd3436dc21a51d76bab73ae68fe06df3
javicps <javicps@gmail.com>
2024-12-15 23:43:36 +0100

    Add styling for menus + css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Professional from './pages/Professional';
 import Writing from './pages/Writing';
 import Laleydeldesierto from './pages/Laleydeldesierto';
+import Menu from './components/Menu';
 
 
 const App = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/professional">Professional</Link></li>
-          <li><Link to="/writing">Writing (ES)</Link></li>
-        </ul>
-      </nav>
 
+      <Menu/>
       <Routes>
         <Route path="/" element={<h1>Welcome to My Page</h1>} />
         <Route path="/professional" element={<Professional />} />
